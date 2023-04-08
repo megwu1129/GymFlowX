c3e03bdd54480552bee43334e78855ac65375e4a
Meg Wu <megwu1129@gmail.com>
2023-04-08 16:41:00 -0500

    refactor all views to class-based view

--- workoutinfo/views.py
+++ workoutinfo/views.py
@@ -1,45 +1,68 @@
 from django.shortcuts import render
+from django.views import View
+
 from workoutinfo.models import Member, Trainer, WorkoutPlan, Workout, NutritionPlan, Membership, Payment
+from django.views.generic import ListView
 
 
-# Create your views here.
-def member_list_view(request):
-    member_list = Member.objects.all()
-    # member_list = Member.objects.none()
-    return render(request, 'workoutinfo/member_list.html', {'member_list': member_list})
+class MemberList(View):
+    def get(self, request):
+        return render(
+            request,
+            'workoutinfo/member_list.html',
+            {'member_list': Member.objects.all()}
+        )
 
 
-def trainer_list_view(request):
-    trainer_list = Trainer.objects.all()
-    # trainer_list = Trainer.objects.none()
-    return render(request, 'workoutinfo/trainer_list.html', {'trainer_list': trainer_list})
+class TrainerList(View):
+    def get(self, request):
+        return render(
+            request,
+            'workoutinfo/trainer_list.html',
+            {'trainer_list': Trainer.objects.all()}
+        )
 
 
-def workoutplan_list_view(request):
-    workoutplan_list = WorkoutPlan.objects.all()
-    # workoutplan_list = WorkoutPlan.objects.none()
-    return render(request, 'workoutinfo/workoutplan_list.html', {'workoutplan_list': workoutplan_list})
+class WorkoutPlanList(View):
+    def get(self, request):
+        return render(
+            request,
+            'workoutinfo/workoutplan_list.html',
+            {'workoutplan_list': WorkoutPlan.objects.all()}
+        )
 
 
-def workout_list_view(request):
-    workout_list = Workout.objects.all()
-    # workout_list = Workout.objects.none()
-    return render(request, 'workoutinfo/workout_list.html', {'workout_list': workout_list})
+class WorkoutList(View):
+    def get(self, request):
+        return render(
+            request,
+            'workoutinfo/workout_list.html',
+            {'workout_list': Workout.objects.all()}
+        )
 
 
-def nutritionplan_list_view(request):
-    nutritionplan_list = NutritionPlan.objects.all()
-    # nutritionplan_list = NutritionPlan.objects.none()
-    return render(request, 'workoutinfo/nutritionplan_list.html', {'nutritionplan_list': nutritionplan_list})
+class NutritionPlanList(View):
+    def get(self, request):
+        return render(
+            request,
+            'workoutinfo/nutritionplan_list.html',
+            {'nutritionplan_list': NutritionPlan.objects.all()}
+        )
 
 
-def membership_list_view(request):
-    membership_list = Membership.objects.all()
-    # membership_list = Membership.objects.none()
-    return render(request, 'workoutinfo/membership_list.html', {'membership_list': membership_list})
+class MembershipList(View):
+    def get(self, request):
+        return render(
+            request,
+            'workoutinfo/membership_list.html',
+            {'membership_list': Membership.objects.all()}
+        )
 
 
-def payment_list_view(request):
-    payment_list = Payment.objects.all()
-    # payment_list = Payment.objects.none()
-    return render(request, 'workoutinfo/payment_list.html', {'payment_list': payment_list})
+class PaymentList(View):
+    def get(self, request):
+        return render(
+            request,
+            'workoutinfo/payment_list.html',
+            {'payment_list': Payment.objects.all()}
+        )
